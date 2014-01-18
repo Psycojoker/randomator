958804201207fd6b2ea90623f1d800805f87ebb5
Laurent Peuch <cortex@worlddomination.be>
2014-01-18 10:25:19 +0100

    [enh] inspiration won't be destroyed, just disabled

--- inspiration/models.py
+++ inspiration/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Inspiration(models.Model):
     name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
